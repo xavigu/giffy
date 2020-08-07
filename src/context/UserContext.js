20665d959a9d4f7a90611cd11e3d90b5e7249f66
xavigu <xavigu91@gmail.com>
2020-08-07 14:32:49 +0200

    logic to getFavs after login

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react';
+import getFavs from 'services/getFavs';
 
 const Context = React.createContext({})
 
@@ -6,6 +7,12 @@ export function UserContextProvider ({children}) {
   // add the return of an arrow function like the initial state
   const [jwt, setJWT] = useState(() => window.sessionStorage.getItem('jwt'));
   const [favs, setFavs] = useState([]);
+
+  useEffect(() => {
+    if (!jwt)  return setFavs([]);
+    getFavs({jwt}).then(setFavs);
+
+  }, [jwt])
 
   return <Context.Provider value={{jwt, favs, setJWT, setFavs}}>
     {children}
