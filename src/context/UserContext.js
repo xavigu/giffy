573e44c9b26cd31c65fa23c5686f5f93c8393f4f
xavigu <xavigu91@gmail.com>
2020-08-07 13:24:50 +0200

    Add and remove jwt in the session storage

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -3,7 +3,8 @@ import React, {useState} from 'react'
 const Context = React.createContext({})
 
 export function UserContextProvider ({children}) {
-  const [jwt, setJWT] = useState(null)
+  // add the return of an arrow function like the initial state
+  const [jwt, setJWT] = useState(() => window.sessionStorage.getItem('jwt'));
 
   return <Context.Provider value={{jwt, setJWT}}>
     {children}
