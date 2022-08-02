b0a078534c93fa8f076029afc17575833e170d66
xavigu <xavigu91@gmail.com>
2022-08-02 20:01:14 +0200

    Updates

---
+++ precache-manifest.d4ae919edc2e619e29aca5a23e121846.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ac785fa6cfea5030163653f5d85868",
+    "url": "/giffy/index.html"
+  },
+  {
+    "revision": "a1882f80dfd14c981f77",
+    "url": "/giffy/static/css/3.44a3b75a.chunk.css"
+  },
+  {
+    "revision": "40ecac3d178e5293025f",
+    "url": "/giffy/static/css/main.cc5ede15.chunk.css"
+  },
+  {
+    "revision": "aa1bd567a7c552e1677a",
+    "url": "/giffy/static/js/2.62c3eeaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giffy/static/js/2.62c3eeaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1882f80dfd14c981f77",
+    "url": "/giffy/static/js/3.5b6ceb1b.chunk.js"
+  },
+  {
+    "revision": "03a91a9be9b5447beb29",
+    "url": "/giffy/static/js/4.d09b1d36.chunk.js"
+  },
+  {
+    "revision": "41112126e0d1ef9f9f86",
+    "url": "/giffy/static/js/5.2ab93d68.chunk.js"
+  },
+  {
+    "revision": "40ecac3d178e5293025f",
+    "url": "/giffy/static/js/main.3c05a992.chunk.js"
+  },
+  {
+    "revision": "c6f917b4bf764e5ff274",
+    "url": "/giffy/static/js/runtime-main.52faefd8.js"
+  }
+]);
