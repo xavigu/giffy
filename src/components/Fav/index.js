840dd975b935fdc4266599d746b7ab6a83229380
xavigu <xavigu91@gmail.com>
2022-08-02 13:23:09 +0200

    Add copy to clipboard button and logic

--- src/components/Fav/index.js
+++ src/components/Fav/index.js
@@ -3,7 +3,7 @@ import useUser from 'hooks/useUser'
 import {useLocation} from 'wouter'
 import './Fav.css'
 
-export default function Fav({id}) {
+export default function Fav({ id, url }) {
   const { isLogged, favs, addFav } = useUser();
   const [, navigate] = useLocation();
   const isFaved = favs.some(favId => favId === id);
@@ -14,11 +14,21 @@ export default function Fav({id}) {
     addFav({id});
   }
 
+  const handleClipboard = () => {
+    navigator.clipboard.writeText(url);
+  }
+
   const [label, emoji] = isFaved ? ['Remove Gif favorites', '❌'] : ['Add Gif favorites', '❤️']
 
   return (
-        <button className='fav-button' onClick={handleClick}>
-          <span aria-label={label} role='img'>{emoji}</span>
-        </button>
+    <>
+      <button className='fav-button' onClick={handleClipboard}>
+        <span aria-label='Copy to clipboard' role='img'>🔗</span>
+      </button>
+
+      <button className='fav-button' onClick={handleClick}>
+        <span aria-label={label} role='img'>{emoji}</span>
+      </button>
+    </>
   )
 }
--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -3,11 +3,12 @@ import {Link} from 'wouter'
 import Fav from 'components/Fav'
 import './Gif.css'
 
+// eslint-disable-next-line react/prop-types
 export default function Gif ({ title, id, url }) {
   return (
     <div className="Gif">
       <div className="Gif-buttons">
-        <Fav id={id}></Fav>
+        <Fav id={id} url={url}></Fav>
       </div>
       <Link to={`/gif/${id}`} className='Gif-link'>
         <h4>{title}</h4>
